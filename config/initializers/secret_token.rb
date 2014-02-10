a8744c83f3e465fc198a4d7f832ce4916eb6a2b4
Daryl Fritz <madcowdzz@gmail.com>
2014-02-09 19:12:51 -0500

    Initialize Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hamlet::Application.config.secret_key_base = '61ebfcccdcddcf212a31b146354536d3a41c3fcd8b4f8050706822c64c471b51d8eae99b55b8927039be1dd703b99043bc2e1c08d53735152b493c0962598bb3'
